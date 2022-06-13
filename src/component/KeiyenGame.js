9bcbddb19742c1cc04e7fa9209b3b68ad21f4136
BonnyPongsumbam <bonnypongsumbam7@gmail.com>
2022-06-13 10:51:40 +0530

    dia

--- src/component/KeiyenGame.js
+++ src/component/KeiyenGame.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./BoardGame.css";
+import { diagonalMove } from "./DiagonalMoves";
 const Kei = (props) => {
   return <span style={{ color: "Red" }}>{props.children}</span>;
 };
@@ -24,14 +25,14 @@ const KeiYenBoard = () => {
   const matrix = new Array(5).fill(0).map(() => new Array(5).fill(0));
   //Fill initial Position
   matrix[1][3] = 25;
-  matrix[2][4] = 11;
+  matrix[4][4] = 11;
   const [gridStatus, setGridStatus] = useState(matrix);
   var InitialRow, InitialColummn, FinalRow, FinalColumn;
   function checkMoveFeasibility() {
        //Ok to move row
     if (
       InitialRow === FinalRow &&
-      Math.abs(FinalColumn - InitialColummn) === 2 &&
+      Math.abs(FinalColumn - InitialColummn) === 1 &&
       matrix[FinalRow][FinalColumn] === 0
     ) {
      
@@ -41,12 +42,12 @@ const KeiYenBoard = () => {
         myMatrix[FinalRow][FinalColumn] = myMatrix[InitialRow][InitialColummn];
         myMatrix[InitialRow][InitialColummn] = 0;
         setGridStatus(myMatrix);
-        console.log(myMatrix);
+      
       }
        //Ok to move column
     } else if (
       InitialColummn === FinalColumn &&
-      Math.abs(FinalRow - InitialRow) === 2 &&
+      Math.abs(FinalRow - InitialRow) === 1 &&
       matrix[FinalRow][FinalColumn] === 0
     ) {
      
@@ -56,8 +57,29 @@ const KeiYenBoard = () => {
         myMatrix[FinalRow][FinalColumn] = myMatrix[InitialRow][InitialColummn];
         myMatrix[InitialRow][InitialColummn] = 0;
         setGridStatus(myMatrix);
+      }}
+      //Ok to move diagonal
+     else if (
+      Math.abs(FinalRow - InitialRow) === 1 &&
+      Math.abs(FinalColumn - InitialColummn) === 1 &&
+      matrix[FinalRow][FinalColumn] === 0
+     
+
+     )
+     
+     {
+        alert("Ok");
+       
+        if (gridStatus[InitialRow][InitialColummn] !== 0) {
+          const myMatrix = gridStatus.slice();
+          myMatrix[FinalRow][FinalColumn] = myMatrix[InitialRow][InitialColummn];
+          myMatrix[InitialRow][InitialColummn] = 0;
+          setGridStatus(myMatrix);
+          console.log(myMatrix);
+        }
+        
       }
-    } else {
+     else {
       alert("Not Ok");
     }
   }
@@ -67,9 +89,9 @@ const KeiYenBoard = () => {
     FinalRow = row;
     FinalColumn = column;
     console.log(row, column);
-    alert("Last Click " + InitialRow + " " + InitialColummn);
-    alert("New Click " + row + " " + column);
-    alert("Trying to Move");
+    // alert("Last Click " + InitialRow + " " + InitialColummn);
+    // alert("New Click " + row + " " + column);
+    // alert("Trying to Move");
     checkMoveFeasibility();
   }
   const DisplayRow = ({ rowitems, rowIndex }) => {
